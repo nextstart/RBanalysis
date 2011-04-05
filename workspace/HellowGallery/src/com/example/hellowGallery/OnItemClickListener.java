74f3817429d85c77fa59765600cbdd59de71539a
joe Developer <joe@dev.org>
2011-04-05 16:56:24 +0200

    two classes for test.
    Signed-off-by: joe Developer <joe@dev.org>

---
+++ workspace/HellowGallery/src/com/example/hellowGallery/OnItemClickListener.java
@@ -0,0 +1,23 @@
+package com.example.hellowGallery;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+public class OnItemClickListener implements
+		android.widget.AdapterView.OnItemClickListener {
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
